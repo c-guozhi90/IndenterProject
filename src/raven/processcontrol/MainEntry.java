08ca707c6bb53503bfac574696e99ede755d1418
Raven Chan <c.guozhi90@gmail.com>
2019-09-22 11:12:43 +0800

    init project 2

--- src/raven/processcontrol/MainEntry.java
+++ src/raven/processcontrol/MainEntry.java
@@ -1,4 +1,17 @@
 package raven.processcontrol;
 
+import raven.wordprocess.FileProcess;
+import raven.wordprocess.Parser;
+
+import java.io.IOException;
+
 public class MainEntry {
+
+    public static void main(String[] args) {
+        try {
+            Parser parser = new Parser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/raven/wordprocess/Parser.java
+++ src/raven/wordprocess/Parser.java
@@ -1,9 +1,8 @@
 package raven.wordprocess;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -11,21 +10,46 @@ import java.util.Stack;
  * the keywords in sql are begin, declare, end, for, if and so on
  */
 public class Parser {
-    private Stack<String> keywords;
+    private Stack<String> keywordStack;
     private FileProcess fileProcess;
     private HashMap<String, String[]> keywordsPairs;
     private String[] startKeys;
+    private boolean startWordFound;
 
-    public Parser() throws FileNotFoundException {
+    public Parser() throws IOException {
         initKeywordsPairs();
-        keywords = new Stack<>();
+        keywordStack = new Stack<>();
 
         fileProcess = new FileProcess(new File(""));
 
     }
 
-    public void wordMatch() {
-        LinkedList<String> words = fileProcess.extractWords();
+    /**
+     * Find the start keyword of sql language and push it into stack
+     */
+    public void findStartWord() throws IOException {
+        String[] words = fileProcess.extractWords();
+        if (keywordsPairs.containsKey(words[0])) { // only examine the first word of each line
+            keywordStack.push(words[0]);
+            startWordFound = true;
+        }
+    }
+
+    /**
+     * Find the end keyword(terminator) of sql language. If found, pop the start word from the stack
+     */
+    public void findEndWord() throws IOException {
+        String strLine = fileProcess.extractLine();
+
+        if (!keywordStack.empty()) {
+            String[] endWords = keywordsPairs.get(keywordStack.peek());
+            for (String eachEndWord : endWords) {
+                if (strLine.indexOf(eachEndWord) > 0) {
+                    keywordStack.pop();
+                    return;
+                }
+            }
+        }
     }
 
     private void initKeywordsPairs() {
